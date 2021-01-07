cb0307ea9caed5dcc18fa0c205ee1a22818fb794
Mark Yoder <mark.yoder@gmail.com>
2021-01-07 11:58:47 -0800

    adding check_dycore_time.py, a little toy python script to crack open the .nc file and check the elapsed timne, which can be used to perorm big runs in sets of smaller sub-runs. it is still a first draft.

---
+++ check_dycore_time.py
@@ -0,0 +1,15 @@
+#!python
+#
+import netCDF4
+import sys
+#
+
+if __name__=="__main__":
+    #print('args: ', sys.argv)
+    fin_name = sys.argv[1]
+    #
+    with netCDF4.Dataset(fin_name, 'r') as fout:
+        #print('** time: {}\n'.format(fout.variables['time'][0]))
+        print(fout.variables['time'][0])
+#
+
