362c759f0da45eecd5073052cf0947a799768379
Mark Yoder <mark.yoder@gmail.com>
2021-10-25 15:43:41 -0700

    cleaned up tree (i hope); removed postprocessing. i hope.

---
+++ scripts/archive/cp_desc_dd.sh
@@ -0,0 +1,24 @@
+cp /scratch2/ddaniela/atm_dycores_check2_gerber09/input/hs_diag_table /scratch2/ddaniela/data/check2_gerber09/model_scripts
+
+cp /scratch2/ddaniela/atm_dycores_check2_gerber09/input/spectral_namelist /scratch2/ddaniela/data/check2_gerber09/model_scripts
+
+cp /scratch2/ddaniela/atm_dycores_check2_gerber09/src/atmos_spectral/init/*.f90 /scratch2/ddaniela/data/check2_gerber09/model_scripts
+
+cp /scratch2/ddaniela/atm_dycores_check2_gerber09/src/atmos_spectral/model/*.f90 /scratch2/ddaniela/data/check2_gerber09/model_scripts
+
+cp /scratch2/ddaniela/atm_dycores_check2_gerber09/src/atmos_param/hs_forcing/hs_forcing.f90 /scratch2/ddaniela/data/check2_gerber09/model_scripts
+
+cp /scratch2/ddaniela/atm_dycores_check2_gerber09/scripts/run_spectral /scratch2/ddaniela/data/check2_gerber09/model_scripts
+
+cp /scratch2/ddaniela/atm_dycores_check2_gerber09/scripts/run_spectral_no_restart /scratch2/ddaniela/data/check2_gerber09/model_scripts
+
+cp /scratch2/ddaniela/atm_dycores_check2_gerber09/src/atmos_spectral/driver/solo/*.f90 /scratch2/ddaniela/data/check2_gerber09/model_scripts
+
+cp /scratch2/ddaniela/atm_dycores_check2_gerber09/src/atmos_solo/*.f90 /scratch2/ddaniela/data/check2_gerber09/model_scripts
+
+cp /scratch2/ddaniela/atm_dycores_check2_gerber09/src/shared/topography/*.f90 /scratch2/ddaniela/data/check2_gerber09/model_scripts
+
+cp /scratch2/ddaniela/atm_dycores_check2_gerber09/src/atmos_spectral/driver/solo /scratch2/ddaniela/data/check2_gerber09/model_scripts
+
+
+
